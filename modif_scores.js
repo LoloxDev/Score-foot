cef2ffd9146b703ee792f0003a393c37443eb1b9
poukette58 <vanessa.geoffroid@infamail.com>
2022-08-05 16:04:42 +0200

    afficher les données

--- modif_scores.js
+++ modif_scores.js
@@ -3,75 +3,6 @@ let cellule_old_value = "";
 let cellule_old_item = "0";
 
 function editabled(item) {
-
-
-
-        document.querySelectorAll(".tab_input")
-            .forEach((element) => {
-                //console.log(element.id+" / "+item.id);
-                /* si l'element n'est pas identique a l'item */
-                if (element.id != item.id || element.id != cellule_old_item) {
-                    /* verifier que l'element n'est pas identique a la verion cliquer avant le changement */
-                    if (cellule_old_item == element.id) {
-                        //console.log('vfffc');
-                        /* si c'est d'une version cliquer avant */
-                        /* on recupere les anciennes informations (en cas d'erreur) */
-                        let value_old = cellule_old_value;
-                        let id_old = element.id;
-                        /* verifier qu'on a bien changer les valeurs */
-                        let modif = element.innerHTML != cellule_old_value;
-                        /* on vide les informations */
-                        cellule_old_value = "";
-                        cellule_old_item = "0";
-                        /* si on a modifier la valeur */
-                        if (modif) {
-                            //console.log('cc');
-                            /* on modifier les valeur dans la base */
-                            if(id_old.startsWith('score')){
-                                let score = [];
-                            for (let index = 1; index < 96; index++) {
-                                let recupScore = document.getElementById("score"+index).innerHTML;
-                                score.push(recupScore);
-                                
-                            }
-                            
-                            localStorage.matchx = JSON.stringify(score);
-                            /*console.log(JSON.parse(localStorage.matchx));*/
-                        }
-                        if(id_old.startsWith('equipe')){
-                            let equipe = [];
-                            for (let index = 1; index < 17; index++) {
-                                let recupEquipe = document.getElementById("equipe"+index).innerHTML;
-                                equipe.push(recupEquipe);
-                                
-                            }
-                            localStorage.equipe = JSON.stringify(equipe);
-                            /*console.log(JSON.parse(localStorage.matchx));*/
-                        }
-
-                        if(id_old.startsWith('box')){
-                            let box = [];
-                            for (let index = 1; index < 30; index++) {
-                                let recupbox = document.getElementById("box"+index).innerHTML;
-                                box.push(recupbox);
-                                
-                            }
-                            localStorage.box = JSON.stringify(box);
-                            /*console.log(JSON.parse(localStorage.matchx));*/
-                        }
-
-
-                    }
-                    /* on desactive la cellule */
-                    element.contentEditable = false;
-
-                }
-            }
-        });
-
-
-}
-
   document.querySelectorAll(".tab_input").forEach((element) => {
     //console.log(element.id+" / "+item.id);
     /* si l'element n'est pas identique a l'item */
@@ -99,7 +30,6 @@ function editabled(item) {
                 "score" + index
               ).innerHTML;
               score.push(recupScore);
-
             }
 
             localStorage.matchx = JSON.stringify(score);
@@ -134,7 +64,7 @@ function editabled(item) {
       }
     }
   });
-
+}
 /**
  * Action si on touche le bouton entrer du clavier.
  */
@@ -157,3 +87,21 @@ function input_edit(e) {
 document.querySelectorAll(".tab_input").forEach((element) => {
   element.addEventListener("dblclick", input_edit);
 });
+
+/*permet d'afficher les données*/
+let scoreTab = JSON.parse(localStorage.matchx);
+for (let index = 0; index < scoreTab.length; index++) {
+    const element = scoreTab[index];
+    document.getElementById(
+        "score" + (index+1)
+      ).innerHTML = element;
+}
+/*console.log(localStorage.equipe);
+console.log(JSON.parse(localStorage.equipe)); un tableau contenu dans une chaine de carctères*/ 
+let equipeTab = JSON.parse(localStorage.equipe);
+for (let index = 0; index < equipeTab.length; index++) {
+    const element = equipeTab[index];
+    document.getElementById(
+        "equipe" + (index+1)
+      ).innerHTML = element;
+}
